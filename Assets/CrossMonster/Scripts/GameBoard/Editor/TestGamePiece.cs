ce88342fc1fbdd5311368fd377473084935fe6c6
jlavoine <joe.lavoine@gmail.com>
2017-02-25 07:12:44 -0500

    Rotating a piece type when it is used

---
+++ Assets/CrossMonster/Scripts/GameBoard/Editor/TestGamePiece.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using NSubstitute;
+using UnityEngine;
+using MyLibrary;
+using System.Collections.Generic;
+
+#pragma warning disable 0219
+#pragma warning disable 0414
+
+namespace CrossMonsters {
+    [TestFixture]
+    public class TestGamePiece : CrossMonstersUnitTest {
+
+        [Test]
+        public void WhenCreatingPiece_PieceTypeMatchesConstructor() {
+            GamePiece systemUnderTest = new GamePiece( 5 );
+
+            Assert.AreEqual( 5, systemUnderTest.PieceType );
+        }
+
+        [Test]
+        public void WhenUsingPiece_PieceTypeRotatesAccordingToGameRules() {
+            GameRules.Instance.GetGamePieceRotation( 0 ).Returns( 3 );
+            GamePiece systemUnderTest = new GamePiece( 0 );
+
+            systemUnderTest.UsePiece();
+
+            Assert.AreEqual( 3, systemUnderTest.PieceType );
+        }
+    }
+}
---
+++ Assets/CrossMonster/Scripts/GameBoard/GamePiece.cs
@@ -0,0 +1,19 @@
+﻿
+namespace CrossMonsters {
+    public class GamePiece : IGamePiece {
+        private int mPieceType;
+        public int PieceType { get { return mPieceType; } set { mPieceType = value; } }
+
+        public GamePiece( int i_pieceType ) {
+            PieceType = i_pieceType;
+        }
+
+        public void UsePiece() {
+            RotatePieceType();
+        }
+
+        private void RotatePieceType() {
+            PieceType = GameRules.Instance.GetGamePieceRotation( PieceType );
+        }
+    }
+}
