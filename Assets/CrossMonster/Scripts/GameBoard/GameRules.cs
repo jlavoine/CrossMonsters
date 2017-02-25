f092689c482d2a91f67d80f78fcd378cd1b45b33
jlavoine <joe.lavoine@gmail.com>
2017-02-25 06:57:50 -0500

    Adding GameRules structure

---
+++ Assets/CrossMonster/Scripts/GameBoard/GameRules.cs
@@ -0,0 +1,18 @@
+﻿
+namespace CrossMonsters {
+    public class GameRules : IGameRules {
+        private static IGameRules mInstance;
+        public static IGameRules Instance {
+            get {
+                if ( mInstance == null ) {
+                    mInstance = new GameRules();
+                }
+                return mInstance;
+            }
+            set {
+                // tests only!
+                mInstance = value;
+            }
+        }
+    }
+}
