22f2a9f30a44ee4aab90df0916a3b34760c335ae
jlavoine <joe.lavoine@gmail.com>
2017-02-25 16:35:51 -0500

    Adding default game rules

--- Assets/CrossMonster/Scripts/GameBoard/GameRules.cs
+++ Assets/CrossMonster/Scripts/GameBoard/GameRules.cs
@@ -19,6 +19,14 @@ namespace CrossMonsters {
         private Dictionary<int, int> mPieceRotations;
         public Dictionary<int, int> PieceRotations { get { return mPieceRotations; } set { mPieceRotations = value; } }
 
+        private List<int> mPieceTypes;
+        public List<int> PieceTypes;
+
+        public GameRules() {
+            PieceTypes = new List<int>() { 0, 1, 2, 3, 4 };
+            PieceRotations = new Dictionary<int, int>() { { 0, 1 }, { 1, 2 }, { 2, 3 }, { 3, 4 }, { 4, 0 } };           
+        }
+
         public int GetGamePieceRotation( int i_pieceType ) {
             if ( PieceRotations.ContainsKey( i_pieceType ) ) {
                 return PieceRotations[i_pieceType];
