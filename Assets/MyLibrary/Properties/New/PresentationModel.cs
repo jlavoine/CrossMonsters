094f3407186b5c42f654b8cb792278942de0fc3d
jlavoine <joe.lavoine@gmail.com>
2017-03-04 06:54:52 -0500

    Missed file in previous commit

--- Assets/MyLibrary/Properties/New/PresentationModel.cs
+++ Assets/MyLibrary/Properties/New/PresentationModel.cs
@@ -4,10 +4,40 @@ namespace MyLibrary {
         private ViewModel mModel;
         public ViewModel ViewModel { get { return mModel; } private set { mModel = value; } }
 
+        private IBusinessModel mBusinessModel;
+
+        // After CrossMonsters, remove this ctor!!!
         public PresentationModel() {
             ViewModel = new ViewModel();
         }
 
-        public virtual void Dispose() { }
+        public PresentationModel( IBusinessModel i_businessModel ) {
+            ViewModel = new ViewModel();
+            SetBusinessModel( i_businessModel );
+            SubscribeToBusinessModelUpdate( true );
+        }
+
+        public void Dispose() {
+            if ( mBusinessModel != null ) {
+                SubscribeToBusinessModelUpdate( false );
+            }
+
+            _Dispose();
+        }
+
+        protected virtual void OnModelUpdated() { }
+        protected virtual void _Dispose() { }
+
+        private void SetBusinessModel( IBusinessModel i_model ) {
+            mBusinessModel = i_model;
+        }
+
+        private void SubscribeToBusinessModelUpdate( bool i_subscribe ) {
+            if ( i_subscribe ) {
+                mBusinessModel.ModelUpdated += OnModelUpdated;
+            } else {
+                mBusinessModel.ModelUpdated -= OnModelUpdated;
+            }
+        }
     }
 }
