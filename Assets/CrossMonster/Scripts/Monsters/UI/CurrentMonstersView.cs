51b70b8ecf93092c094c6df6f7bafd41c4cbcc48
jlavoine <joe.lavoine@gmail.com>
2017-03-12 07:44:05 -0400

    Setting current monsters in game scene from the session data

--- Assets/CrossMonster/Scripts/Monsters/UI/CurrentMonstersView.cs
+++ Assets/CrossMonster/Scripts/Monsters/UI/CurrentMonstersView.cs
@@ -13,6 +13,9 @@ namespace CrossMonsters {
         [Inject]
         IGameManager GameManager;
 
+        [Inject]
+        ICurrentDungeonGameManager CurrentDungeonData;
+
         void Start() {
             List<IGameMonster> monsters = GetMonsters();
             MonsterManager.SetMonsters( monsters );
@@ -45,30 +48,9 @@ namespace CrossMonsters {
             MonsterView monsterView = monsterObject.GetComponent<MonsterView>();
             monsterView.Init( new MonsterPM( i_monster ) );
         }
-
-        // TODO just for testing
+        
         private List<IGameMonster> GetMonsters() {
-            List<IGameMonster> monsters = new List<IGameMonster>();
-            
-            monsters.Add( GetMonsterDataTEMP( "Blob", 25, new List<int>() { 0, 0 }, 10, 0, 5000 ) );
-            monsters.Add( GetMonsterDataTEMP( "Dragon", 50, new List<int>() { 1, 1 }, 15, 1, 10000 ) );
-            monsters.Add( GetMonsterDataTEMP( "Blob", 25, new List<int>() { 3, 2 }, 10, 0, 5000 ) );
-            monsters.Add( GetMonsterDataTEMP( "Blob", 25, new List<int>() { 2, 0 }, 10, 0, 5000 ) );
-            monsters.Add( GetMonsterDataTEMP( "Goblin", 25, new List<int>() { 3, 3 }, 10, 0, 5000 ) );
-
-            return monsters;
-        }
-
-        private GameMonster GetMonsterDataTEMP( string i_id, int i_hp, List<int> i_combo, int damage, int damageType, long attackRate ) {
-            MonsterData data = new MonsterData();
-            data.Id = i_id;
-            data.MaxHP = i_hp;
-            data.AttackCombo = i_combo;
-            data.Damage = damage;
-            data.DamageType = damageType;
-            data.AttackRate = attackRate;
-
-            return new GameMonster( data );
-        }
+            return CurrentDungeonData.Monsters;
+        }      
     }
 }
