60bfcdb580d2891a86afd2b950006ff5760ff4f6
jlavoine <joe.lavoine@gmail.com>
2017-03-12 07:43:49 -0400

    Injecting additional classes into DungeonLoader

--- Assets/CrossMonster/Scripts/DungeonMap/DungeonLoader.cs
+++ Assets/CrossMonster/Scripts/DungeonMap/DungeonLoader.cs
@@ -1,16 +1,41 @@
 ﻿using Zenject;
 using MyLibrary;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using Newtonsoft.Json;
 
 namespace CrossMonsters {
     public class DungeonLoader {
         readonly ILoadingScreenPM mLoadingPM;
+        readonly IBackendManager mBackendManager;
+        readonly ICurrentDungeonGameManager mCurrentDungeonData;
+        readonly ISceneManager mSceneManager;
 
-        public DungeonLoader( ILoadingScreenPM i_loadingPM ) {
+        public DungeonLoader( ILoadingScreenPM i_loadingPM, IBackendManager i_backend, ICurrentDungeonGameManager i_currentDungeon, ISceneManager i_sceneManager ) {
             mLoadingPM = i_loadingPM;
+            mBackendManager = i_backend;
+            mCurrentDungeonData = i_currentDungeon;
+            mSceneManager = i_sceneManager;
         }
 
         public void OnClick() {
             mLoadingPM.Show();
+            GetGameSessionFromServer();
+        }
+
+        private void GetGameSessionFromServer() {
+            Dictionary<string, string> cloudParams = new Dictionary<string, string>();
+            cloudParams.Add( "AreaId", "0" );
+            cloudParams.Add( "DungeonId", "0" );
+
+            mBackendManager.GetBackend<IBasicBackend>().MakeCloudCall( "getDungeonGameSession", cloudParams, ( result ) => {
+                OnDungeonGameSessionResponse( JsonConvert.DeserializeObject<DungeonGameSessionData>( result["data"] ) );
+            } );
+        }
+
+        public void OnDungeonGameSessionResponse( IDungeonGameSessionData i_data ) {            
+            mCurrentDungeonData.SetData( i_data );
+            mSceneManager.LoadScene( "Playground" );
         }
 
         public class Factory : Factory<DungeonLoader> { }
--- Assets/CrossMonster/Scripts/DungeonMap/Editor/TestDungeonLoader.cs
+++ Assets/CrossMonster/Scripts/DungeonMap/Editor/TestDungeonLoader.cs
@@ -11,14 +11,61 @@ using Zenject;
 namespace CrossMonsters {
     public class TestDungeonLoader : ZenjectUnitTestFixture {
 
+        [Inject]
+        ILoadingScreenPM MockLoadingPM;
+
+        [Inject]
+        IBackendManager MockBackendManager;
+
+        [Inject]
+        ICurrentDungeonGameManager MockCurrentDungeon;
+
+        [Inject]
+        ISceneManager MockSceneManager;
+
+        [Inject]
+        DungeonLoader systemUnderTest;
+
+        [SetUp]
+        public void CommonInstall() {
+            Container.Bind<ILoadingScreenPM>().FromInstance( Substitute.For<ILoadingScreenPM>() );
+            Container.Bind<IBackendManager>().FromInstance( Substitute.For<IBackendManager>() );
+            Container.Bind<ICurrentDungeonGameManager>().FromInstance( Substitute.For<ICurrentDungeonGameManager>() );
+            Container.Bind<ISceneManager>().FromInstance( Substitute.For<ISceneManager>() );
+            Container.Bind<DungeonLoader>().AsSingle();       
+            Container.Inject( this );
+        }
+
         [Test]
         public void OnClick_LoadingScreenShowIsCalled() {
-            ILoadingScreenPM mockLoading = Substitute.For<ILoadingScreenPM>();
-            DungeonLoader systemUnderTest = new DungeonLoader( mockLoading );
-
             systemUnderTest.OnClick();
 
-            mockLoading.Received().Show();
+            MockLoadingPM.Received().Show();
+        }
+
+        [Test]
+        public void OnClick_BackendCloudCallMade_ToGetGameSession() {
+            IBasicBackend mockBackend = Substitute.For<IBasicBackend>();
+            MockBackendManager.GetBackend<IBasicBackend>().Returns( mockBackend );
+            systemUnderTest.OnClick();
+
+            mockBackend.Received().MakeCloudCall( "getDungeonGameSession", Arg.Any<Dictionary<string, string>>(), Arg.Any<Callback<Dictionary<string, string>>>() );
+        }
+
+        [Test]
+        public void WhenReceivingDungeonData_SetsDataOnCurrentDungeonManager() {
+            IDungeonGameSessionData mockData = Substitute.For<IDungeonGameSessionData>();
+            systemUnderTest.OnDungeonGameSessionResponse( mockData );
+
+            MockCurrentDungeon.Received().SetData( mockData );
+        }
+
+        [Test]
+        public void WhenReceivingDungeonData_GameSceneIsLoaded() {
+            IDungeonGameSessionData mockData = Substitute.For<IDungeonGameSessionData>();
+            systemUnderTest.OnDungeonGameSessionResponse( mockData );
+
+            MockSceneManager.Received().LoadScene( "Playground" );
         }
     }
 }
