49b2e413dcbf29daa7b85baa54be75e3e639e240
jlavoine <joe.lavoine@gmail.com>
2017-03-12 07:42:14 -0400

    Adding access method for getting monster data

--- Assets/CrossMonster/Scripts/Monsters/IMonsterDataManager.cs
+++ Assets/CrossMonster/Scripts/Monsters/IMonsterDataManager.cs
@@ -4,5 +4,7 @@ using System.Collections.Generic;
 namespace CrossMonsters {
     public interface IMonsterDataManager {
         void Init( IBasicBackend i_backend );
+
+        IMonsterData GetData( string i_id );
     }
 }
