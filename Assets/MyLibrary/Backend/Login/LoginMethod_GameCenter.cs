9d573c84cb5869743256a920c5195c46fd6b72f3
jlavoine <joe.lavoine@gmail.com>
2017-03-29 09:47:18 -0400

    Log in with Game Center
    This is untested at the moment.

---
+++ Assets/MyLibrary/Backend/Login/LoginMethod_GameCenter.cs
@@ -0,0 +1,13 @@
+﻿using Zenject;
+using UnityEngine;
+
+namespace MyLibrary {
+    public class LoginMethod_GameCenter : ILoginMethod_DeviceId {
+        [Inject]
+        IBackendManager Backend;
+
+        public void Authenticate() {
+            Backend.GetBackend<IBasicBackend>().Authenticate( Social.localUser.id );
+        }
+    }
+}
