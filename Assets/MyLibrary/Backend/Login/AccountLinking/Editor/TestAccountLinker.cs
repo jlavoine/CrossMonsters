daf83bbcb3bfdcbfc83745ae441769d4cefc3ea1
jlavoine <joe.lavoine@gmail.com>
2017-04-11 20:39:31 -0400

    Adding tests for account linker

---
+++ Assets/MyLibrary/Backend/Login/AccountLinking/Editor/TestAccountLinker.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using NSubstitute;
+using Zenject;
+
+#pragma warning disable 0219
+#pragma warning disable 0414
+
+
+namespace MyLibrary {
+    [TestFixture]
+    public class TestAccountLinker : ZenjectUnitTestFixture {
+        [Inject]
+        IGoogleLinker GoogleLinker;
+
+        [Inject]
+        AccountLinker systemUnderTest;
+
+        [SetUp]
+        public void CommonInstall() {
+            Container.Bind<IGoogleLinker>().FromInstance( Substitute.For<IGoogleLinker>() );
+            Container.Bind<AccountLinker>().AsSingle();
+            Container.Inject( this );
+        }
+
+        [Test]
+        public void WhenLinkingWithGoogle_GoogleLinkerAttemptsToLink_NoForce() {
+            systemUnderTest.AttemptToLink( LoginMethods.Google, ( result ) => { } );
+
+            GoogleLinker.Received().AttemptLink( Arg.Any<Callback<AccountLinkResultTypes>>(), false );
+        }
+
+        [Test]
+        public void WhenForceLinkingWithGoogle_GoogleLinkerAttemptsToLink_WithForce() {
+            systemUnderTest.AttemptForceLink( LoginMethods.Google, ( result ) => { } );
+
+            GoogleLinker.Received().AttemptLink( Arg.Any<Callback<AccountLinkResultTypes>>(), true );
+        }
+    }
+}
