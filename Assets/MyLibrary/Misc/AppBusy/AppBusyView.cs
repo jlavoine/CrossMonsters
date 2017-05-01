bc446ce078168b14c8f058b7a393bdd2d2634ba1
jlavoine <joe.lavoine@gmail.com>
2017-05-01 17:35:38 -0400

    Adding really simple "busy" view
    For when app makes server calls and must wait

---
+++ Assets/MyLibrary/Misc/AppBusy/AppBusyView.cs
@@ -0,0 +1,20 @@
+﻿using Zenject;
+
+namespace MyLibrary {
+    public class AppBusyView : GroupView {
+        [Inject]
+        IAppBusyPM PM;
+
+        void Start() {
+            SetModel( PM.ViewModel );
+        }
+
+        public void ShowView() {
+            PM.Show();
+        }
+
+        public void HideView() {
+            PM.Hide();
+        }
+    }
+}
