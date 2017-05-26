712704cb8d5971990c8946054bb3955f609bef7c
jlavoine <joe.lavoine@gmail.com>
2017-05-26 07:47:04 -0400

    Adding script to turn animators on/off from a property

---
+++ Assets/MyLibrary/Properties/New/SetAnimatorEnabledView.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace MyLibrary {
+    public class SetAnimatorEnabledView : PropertyView {
+        private Animator mAnimator;
+        public Animator Animator {
+            get {
+                if ( mAnimator == null ) {
+                    mAnimator = GetComponent<Animator>();
+                }
+
+                return mAnimator;
+            }
+
+            set {
+                mAnimator = value;
+            }
+        }
+
+        public override void UpdateView() {
+            bool isOn = GetValue<bool>();
+
+            if ( Animator ) {
+                Animator.enabled = isOn;
+            }
+        }
+    }
+}
