29f3fdd0ba71824b0440c667c964f397a0b94a86
jlavoine <joe.lavoine@gmail.com>
2017-06-03 16:12:40 -0400

    Adding expedition unit custom data
    For deserializing from item custom data

---
+++ Assets/CrossMonster/Scripts/Player/ExpeditionUnits/ExpeditionUnitCustomData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace MonsterMatch {
+    public class ExpeditionUnitCustomData : IExpeditionUnitCustomData {
+        public Dictionary<string, int> Effects;
+
+        public bool HasEffect( string i_effectId ) {
+            return Effects.ContainsKey( i_effectId );
+        }
+
+        public int GetEffect( string i_effectId ) {
+            if ( HasEffect( i_effectId ) ) {
+                return Effects[i_effectId];
+            } else {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Assets/CrossMonster/Scripts/Player/ExpeditionUnits/IExpeditionUnitCustomData.cs
@@ -0,0 +1,8 @@
+﻿
+namespace MonsterMatch {
+    public interface IExpeditionUnitCustomData {
+        bool HasEffect( string i_effectId );
+
+        int GetEffect( string i_effectId );
+    }
+}
