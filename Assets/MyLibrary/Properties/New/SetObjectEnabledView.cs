187eb525ee7618ce98d97c3dfd6ee2921918c4f0
jlavoine <joe.lavoine@gmail.com>
2017-06-04 07:43:33 -0400

    Disabling the OFF state of a game piece tile when it's ON
    Added this functionality to the SetObjectEnabledView script

--- Assets/MyLibrary/Properties/New/SetObjectEnabledView.cs
+++ Assets/MyLibrary/Properties/New/SetObjectEnabledView.cs
@@ -3,17 +3,35 @@ using UnityEngine;
 
 namespace MyLibrary {
     public class SetObjectEnabledView : PropertyView {
+        public enum EnabledStates {
+            Enabled,
+            Disabled
+        }
+
+        public EnabledStates SetToState;
         public List<GameObject> Objects;
 
         public override void UpdateView() {
             bool value = GetValue<bool>();
+            bool activeState = GetActiveState( value );
 
-            SetObjectsEnabledState( value );
+            SetObjectsEnabledState( activeState );
         }
 
         private void SetObjectsEnabledState( bool i_enabled ) {
             foreach ( GameObject obj in Objects ) {
                 obj.SetActive( i_enabled );
+            }
+        }
+
+        private bool GetActiveState( bool i_propertyValue ) {
+            // this method may be a little confusing. This script allows the enabling or disabling of an object
+            // based on a property. If the property is TRUE, the state of the object will be set to what SetToState is.
+            // if the property is FALSE, it's the opposite.
+            if ( i_propertyValue ) {
+                return SetToState == EnabledStates.Enabled;
+            } else {
+                return SetToState != EnabledStates.Enabled;
             }
         }
     }
